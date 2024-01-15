c5dc375bc670d4301151312320e66faf4a7e9756
ashish4549 <129039812+ashish4549@users.noreply.github.com>
2024-01-15 18:38:56 +0530

    Create TaskNumber.sol

---
+++ contracts/TaskNumber.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract TaskNumber {
+    //create a variable with name taskNumber with initial value of 10
+    
+    //create function called setTask() with one input value when function call
+it will change the taskNumber value to new value
+
+    //create a function called getTask() that return current taskNumber
+    
+}

9fe61b4a68cfd2a661d7e4c1ed67a78f31f3c2b9
ashish4549 <129039812+ashish4549@users.noreply.github.com>
2024-01-15 18:51:28 +0530

    Update TaskNumber.sol

--- contracts/TaskNumber.sol
+++ contracts/TaskNumber.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.4;
 contract TaskNumber {
     //create a variable with name taskNumber with initial value of 10
     
-    //create function called setTask() with one input value when function call
-it will change the taskNumber value to new value
+    //create function called setTask() with one input value when function call it will change the taskNumber value to new value
 
     //create a function called getTask() that return current taskNumber
     
